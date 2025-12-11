507e3d92e1b7043d0c96f2457081f0fb7176a04f
root <root@server1.meglertipset.no>
2025-12-11 10:20:21 +0000

    add

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import store from "./store";
 function App() {
   return (
     <Provider store={store}>
-      <Router>
+      <Router  basename="/admin">
         <Routes />
       </Router>
     </Provider>
